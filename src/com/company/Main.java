05e378ea517e65943bb74e1ad740d20b16cb76e1
kobejenkins11 <kobe.jenkins.2022@student.lhschools.org>
2020-02-25 11:55:24 -0500

    intelij commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("My name is ");
+        System.out.println("Kobe Jenkins");
+
+        System.out.println("last summer I went to ethiopia ");
+        System.out.println("I stayed there for one month and a week");
+        System.out.println("When I came back I just chilled at home");
+    }
+
+}
